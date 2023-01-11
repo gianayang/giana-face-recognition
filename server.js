2f6ff2ff725eed3717038360757f86e777d6c9e6
gianayang <gianayang31@gmail.com>
2023-01-11 14:24:17 -0500

    adding database connections

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user:'',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
