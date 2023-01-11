051acee6273db00808ddd5bf87e4915f69f60702
gianayang <gianayang31@gmail.com>
2023-01-11 16:06:51 -0500

    modifying db queries

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,13 +3,23 @@ const handleSignin = (req, res, db, bcrypt)=> {
     if (!email || !password) {
         return res.status(400).json('incorrect login submission')
     }
-    console.log(email, password);
-    db.query(`SELECT email, hash FROM login WHERE email = "${email}";`, (err, data) => {
+    // console.log(email, password);
+    const query1 = {
+        text: 'SELECT email, hash FROM login WHERE email = $1',
+        values: [email],
+    }
+
+    const query2 = {
+        text: 'SELECT * FROM users WHERE email = $1',
+        values: [email],
+    }
+
+    db.query(query1, (err, data) => {
         if (err) throw err;
         const isValid = bcrypt.compareSync(password, data[0].hash);
         res.json(user[0])
         if (isValid) {
-            return db.query(`SELECT * FROM users WHERE email = "${email}";`, (err, user) => {
+            return db.query(query2, (err, user) => {
                 if (err) throw err;
                 res.json(user[0])
             })
