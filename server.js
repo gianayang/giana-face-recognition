471b57469fda9e2909ee362621634b8a4bae9dd3
gianayang <gianayang31@gmail.com>
2023-01-11 14:15:22 -0500

    change port to deploy-port

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.put('/image', (req, res) => {
     .catch(err=> res.status(400).json('unable to get image entries'))
 })
 
-// app.listen(process.env.PORT ||3000 , () => {
-//     console.log(`app is running on port ${process.env.PORT}`);
-// })
-app.listen(3000 , () => {
+app.listen(process.env.PORT ||3000 , () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
+// app.listen(3000 , () => {
+//     console.log(`app is running on port ${process.env.PORT}`);
+// })
